d95faaa4f68e1b8abd065311325e9f25d962e723
0nza1101 <onza1101@gmail.com>
2022-05-13 00:02:57 +0200

    fix: cli refacto

--- cli.ts
+++ cli.ts
@@ -1,36 +1,29 @@
 import { parse } from "https://deno.land/std@0.138.0/flags/mod.ts";
-import { wakeDyno } from "./mod.ts";
+import { Options, wakeDyno } from "./mod.ts";
 
 if (import.meta.main) {
   console.log('Args', parse(Deno.args));
   const args = parse(Deno.args);
 
   if (args._.length > 0) {
+    const options: Options = {
+      interval: args.interval ?? 29,
+      ...(args.stopStart && args.stopEnd) && {
+        stopTimes: {
+          start: args.start,
+          end: args.end
+        }
+      }
+    }
     const url = args.url;
     if (url) {
-      wakeDyno(url, {
-        interval: args.interval ?? 29,
-        ...(args.stopStart && args.stopEnd) && {
-          stopTimes: {
-            start: args.start,
-            end: args.end
-          }
-        }
-      });
+      wakeDyno(url, options);
     }
 
     let urls = args.urls;
     if (urls) {
       urls = args.urls.split(',');
-      wakeDyno(urls, {
-        interval: args.interval ?? 29,
-        ...(args.stopStart && args.stopEnd) && {
-          stopTimes: {
-            start: args.start,
-            end: args.end
-          }
-        }
-      });
+      wakeDyno(urls, options);
     }
   } else {
     console.log('Usage:\n$ heroku-awakener --url <url> --interval <interval> --stopStart <start> --stopEnd <end>')
